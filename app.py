04c430507ef904bf75e76f375f3da8421b13eaf9
Nilton_Ccahuana <niltonromario369@gmail.com>
2026-02-14 09:08:22 -0500

    Se crea las primeras variables

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
 """
 Sistema de Control de Ventas Diarias para una MYPE en Perú
 """
+total_dia=0
+mayor_cantidad=0
+producto_mas_vendido=""
+meta_diaria=str(input("Ingrese la meta diaria por favor: "))
