b13ad6f8c00c28aabb2258c4030d922ec263b0f2
Nilton_Ccahuana <niltonromario369@gmail.com>
2026-02-14 12:01:16 -0500

    Agregar registro de ventas diarias, cálculo de ingresos totales, producto más vendido y verificación de meta diaria

--- app.py
+++ app.py
@@ -4,4 +4,31 @@ Sistema de Control de Ventas Diarias para una MYPE en Perú
 total_dia=0
 mayor_cantidad=0
 producto_mas_vendido=""
-meta_diaria=str(input("Ingrese la meta diaria por favor: "))
+meta_diaria=int(input("Ingrese la meta diaria por favor: "))
+
+while True:
+    continuar=input("¿Desea registrar una venta?(si/no): ")
+    if continuar.lower() == "no":
+        break
+    nombre_producto=input("Escriba el nombre del producto: ")
+    precio_unitario=float(input("Ingrese su precio unitario: "))
+    cantidad=int(input("Ingrese la cantidad por favor: "))
+
+    ingreso=precio_unitario*cantidad
+    total_dia+=ingreso
+    
+    if cantidad>mayor_cantidad:
+        mayor_cantidad=cantidad
+        producto_mas_vendido=nombre_producto
+    
+print(total_dia)
+print(producto_mas_vendido)
+
+if total_dia>=meta_diaria:
+    print("Meta alcanzada")
+else:
+    print("Meta no alcanzada")
+
+
+
+
